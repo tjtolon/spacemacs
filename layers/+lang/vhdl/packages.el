51b5f62eec5d68b66f01c274c09b536d67bc005f
Tatu Tolonen <tatu.tolonen@gmail.com>
2018-09-14 11:40:18 +0300

    more vhdl mode keybindings

--- layers/+lang/vhdl/packages.el
+++ layers/+lang/vhdl/packages.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
 ;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; Author: Tatu Tolonen <tatu.tolonen@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
@@ -24,11 +24,33 @@
       (spacemacs/declare-prefix-for-mode 'vhdl-mode "mi" "indent")
       (spacemacs/declare-prefix-for-mode 'vhdl-mode "mu" "update")
       (spacemacs/declare-prefix-for-mode 'vhdl-mode "mc" "compile")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "mx" "text")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "mf" "fill")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "ml" "line")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "mp" "port")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "ma" "align")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "ms" "subprog")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "mu" "update SL")
+
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "mt" "template")
+      (spacemacs/declare-prefix-for-mode 'vhdl-mode "mta" "A")
       (spacemacs/set-leader-keys-for-major-mode 'vhdl-mode
         "ib" 'vhdl-indent-buffer
         "ir" 'vhdl-indent-region
         "us" 'vhdl-update-sensitivity-list
         "uS" 'vhdl-update-sensitivity-list-buffer
-        "cc" 'vhdl-compile)
+        "cc" 'vhdl-compile
+        "xc" 'vhdl-fix-case-buffer
+        "ff" 'vhdl-fill-list
+        "fg" 'vhdl-fill-group
+        "tal" 'vhdl-template-alias
+        "tar" 'vhdl-template-architecture
+        "tag" 'vhdl-template-argument-list
+        "tay" 'vhdl-template-array
+        "tcc" 'vhdl-template-component
+        "tci" 'vhdl-template-component-inst
+        "tcd" 'vhdl-template-component-decl
+
+        )
 
       )))
